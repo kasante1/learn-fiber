b967f10fb9a09964cf65e3c68ae1a12eed238783
K Asante <73370911+kasante1@users.noreply.github.com>
2024-04-27 18:34:13 +0000

    ft-book-endpoints

--- main.go
+++ main.go
@@ -4,14 +4,21 @@ import (
     "log"
 
     "github.com/gofiber/fiber/v2"
+	"github.com/kasante1/learn-fiber/book"
 )
 
 func helloWorld(c *fiber.Ctx) error {
-	return c.SendString("hello world")
+	return c.SendString("Hello, World 👋!")
 }
 
 func setUpRoutes(app *fiber.App) {
 	app.Get("/", helloWorld)
+
+	app.Get("/api/v1/book", book.GetBooks)
+	app.Get("/api/v1/book/:id", book.GetBook)
+	app.Post("/api/v1/book", book.NewBook)
+	app.Delete("/api/v1/book/:id", book.DeleteBook)
+
 }
 
 func main() {
